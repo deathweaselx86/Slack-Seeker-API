95c2592a9dd0d9e18563cd4e10eb6891fee0f9f5
Jahnavi Bantupalli <jahnavi.bantupalli@optimizely.com>
2019-07-15 15:54:31 -0700

    changed the endpoint to return sample msg

--- src/app.py
+++ src/app.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask import jsonify
 from src.config import app_config
+import json
+import os
+import sys
 
 
 def create_app(env_name):
@@ -14,7 +18,10 @@ def create_app(env_name):
 
     @app.route('/', methods=['GET'])
     def get_all():
-        return "hello"
+
+        with open(os.path.join(sys.path[0], "src/data/list.json"), "r") as f:
+            data =json.load(f)
+        return jsonify(data)
 
 
     return app
