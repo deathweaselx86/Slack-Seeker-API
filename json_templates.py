219b8f982183e6a92958fe111c1dedb10cdd9df8
Brandon <brandon.david@optimizely.com>
2019-07-16 16:00:05 -0700

    add more json templates

--- json_templates.py
+++ json_templates.py
@@ -7,6 +7,18 @@ def seeker_tags():
 def seeker_tag(tag):
     return '''[{"type": "section","text": {"type": "mrkdwn","text": "Here are the Slack messages with the tag `{''' + tag + '''}`. Help your coworkers out and leave a thumbs up on the messages that were helpful!"}},]'''
 
+def seeker_save(message_URL, description, tag):
+    return '''[{"type": "section","text": {"type": "mrkdwn","text": "Alright! Saved "*''' + description + ''':* ''' + message_URL + ''' with tag `''' + tag + '''`.}},]'''
+
+def seeker_save_msgURL():
+    return '''[{"type": "section","text": {"type": "mrkdwn","text": "Time to save a message to Seeker! What's the Slack message URL?"}},]'''
+
+def seeker_save_desc():
+    return '''[{"type": "section","text": {"type": "mrkdwn","text": "Okay! please provide a short description (a sentence or less) of the message."}},]'''
+
+def seeker_save_tag():
+    return '''{"type": "section","text": {"type": "mrkdwn","text": "Great! Now assign a tag to this Slack message.If the tag hasn't been created yet, we'll create it for you."}},]'''
+
 ### Example for how a Slack message's information should be displayed
 # {
 #     "type": "section",
@@ -24,62 +36,3 @@ def seeker_tag(tag):
 #         "value": "some_value"
 #     }
 # }
-
-
-# seeker_tag_tag = [    
-#     {
-#         "type": "section",
-#         "text": {
-#             "type": "mrkdwn",
-#             "text": "Here are the Slack messages with the tag `monolith_setup`. Help your coworkers out and leave a thumbs up on the messages that were helpful!"
-#         }
-#     },
-#     {
-#         "type": "section",
-#         "text": {
-#             "type": "mrkdwn",
-#             "text": "*Monolith bug with secrets:* https://optimizely.slack.com/archives/SDHIOGH"
-#         },
-#         "accessory": {
-#             "type": "button",
-#             "text": {
-#                 "type": "plain_text",
-#                 "text": ":+1:",
-#                 "emoji": true
-#             },
-#             "value": "some_value"
-#         }
-#     },
-#     {
-#         "type": "section",
-#         "text": {
-#             "type": "mrkdwn",
-#             "text": "*Dependencies for monolith:* https://optimizely.slack.com/archives/EEIRUG"
-#         },
-#         "accessory": {
-#             "type": "button",
-#             "text": {
-#                 "type": "plain_text",
-#                 "text": ":+1:",
-#                 "emoji": true
-#             },
-#             "value": "click_me_123"
-#         }
-#     },
-#     {
-#         "type": "section",
-#         "text": {
-#             "type": "mrkdwn",
-#             "text": "*Monolith thing:* https://optimizely.slack.com/archives/SDGVERIUBG"
-#         },
-#         "accessory": {
-#             "type": "button",
-#             "text": {
-#                 "type": "plain_text",
-#                 "text": ":+1:",
-#                 "emoji": true
-#             },
-#             "value": "click_me_123"
-#         }
-#     }
-# ]
