58ba3f630cdf322c24e43fb8419071994a1137db
Trent Robbins <trent.robbins@optimizely.com>
2019-07-16 14:31:20 -0700

    Added some control flow for dict commands.

--- app/__init__.py
+++ app/__init__.py
@@ -7,25 +7,44 @@ import flask
 from flask import request
 import sys
 
+import json_templates
+
 
 # app initiliazation
 app = Flask(__name__)
 env_name = os.getenv('FLASK_ENV')
 app.config.from_object(app_config[env_name])
 
-
 @app.route('/', methods=['GET'])
 def get_all():
     return "hello world"
-
 
 @app.route('/testjson', methods=['GET','POST'])
 def get_json():
 
     dirname = os.path.dirname(__file__)
     payload = request.get_data(as_text=True)
-    response = flask.make_response(payload,200)
 
+    # testing shim
+    parsed_payload = {'command': 'tags'}
+
+    # repeat for each command, we can fix the structure later
+    if parsed_payload['command'] == 'tags':
+        tag_json_template = json_template.seeker_tags()
+        tag_list = ['tag1', 'tag2', 'tag3']
+        response_payload = jsonify(tag_json_template)
+
+    # repeat for each command, we can fix the structure later
+    if parsed_payload['command'] == 'help':
+        help_json_template = json_template.seeker_help()
+        response_payload = jsonify(taglist)
+
+
+    # if command not recognized
+    else:
+        response_payload = u"Invalid command: {}".format(parsed_payload['command'])
+
+    response = flask.make_response(payload, 200)
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Content-Type'] = 'application/json'
 
