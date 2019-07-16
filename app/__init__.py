bd5d4386d37697539c351412606e88336f717efb
Jahnavi Bantupalli <jahnavi.bantupalli@optimizely.com>
2019-07-16 13:49:19 -0700

    added return payload

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from app.config import app_config
 import json
 import os
 import flask
+from flask import request
 import sys
 
 
@@ -21,10 +22,10 @@ def get_all():
 @app.route('/testjson', methods=['GET','POST'])
 def get_json():
     dirname = os.path.dirname(__file__)
-
+    payload = request.get_data()
     with open(os.path.join(dirname+'/data/list.json'), "r") as f:
         data = json.load(f)
     response = flask.make_response(jsonify(data),200)
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Content-Type'] = 'application/json'
-    return response
+    return payload
