14ddfb39ffbea60e9a77f00530efd694c0fb1910
Jahnavi Bantupalli <jahnavi.bantupalli@optimizely.com>
2019-07-16 10:07:09 -0700

    placed app outside

--- run.py
+++ run.py
@@ -3,8 +3,9 @@ import os
 
 from src.app import create_app
 
+env_name = os.getenv('FLASK_ENV')
+app = create_app(env_name)
+
 if __name__ == '__main__':
-  env_name = os.getenv('FLASK_ENV')
-  app = create_app(env_name)
   # run app
   app.run()
