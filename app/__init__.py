138d2dd4f2ce6029f327173ae419b3cf6f4e0bc9
Jerry Yuan Chen <yuan.chen@optimizely.com>
2019-07-17 10:37:31 -0700

    enable help command

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ import flask
 from flask import request
 from urllib.parse import parse_qs
 import sys
-import parser
+import app.text_parser as text_parser
 
 import json_templates
 
@@ -31,22 +31,18 @@ def get_payload():
     response = flask.make_response(payload, 200)
     return response
 
-    return response
-
 @app.route('/testjson', methods=['GET','POST'])
 def get_json():
 
-    payload = request.get_data(as_text=True)
     # payload = "token=nB2bAolyYp4uDT0hoqUyfVyv&team_id=T02V23TD2&team_domain=optimizely&channel_id=CLF247QJK&channel_name=hackweek2019-seeker&user_id=UGBCTP0AZ&user_name=trent.robbins&command=%2Fseeker-payload&text=&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT02V23TD2%2F690346166849%2FqeJF824OcWVD8tuLhmvwlYvt&trigger_id=696685010704.2988129444.7f616d9c52435a42645e726f26a42269"
 
     # resp wrapped in dict because it's sometimes an array
     # we should catch if there is no text in the command e.g. "/command <text>" where <text> is empty
     # because that is the case where it could just be a dict in a dict
 
-    resp = {'key': parse_qs(payload)}
     #response = flask.make_response(jsonify(resp),200)
 
-    parsed_payload = parser.parse(request.form['text'])
+    parsed_payload = text_parser.parse(request.form['text'])
 
     # repeat for each command, we can fix the structure later
 
@@ -55,12 +51,12 @@ def get_json():
 
     # repeat for each command, we can fix the structure later
     if parsed_payload['command'] == 'help' or parsed_payload['command'] == None:
-        help_json_template = json_template.seeker_help()
+        help_json_template = json_templates.seeker_help()
         response_payload = jsonify(help_json_template)
 
     # repeat for each command, we can fix the structure later
     elif parsed_payload['command'] == 'tags':
-        tag_json_template = json_template.seeker_tags()
+        tag_json_template = json_templates.seeker_tags()
         tag_list = ['tag1', 'tag2', 'tag3']
         response_payload = jsonify(tag_json_template)
 
@@ -70,12 +66,6 @@ def get_json():
         # TODO: this probably fails due being dict(dict( instead of dict(list(dict -- handle this!
         response_payload = u'Invalid command: {}'.format(parsed_payload['command'])
     '''
-    print(resp)
-    if resp['key']['command'] == '/seeker':
-        response_payload = u'Display seeker help if no parameter is provided... you provided the parameter {}'.format(parsed_payload['command'])
-    else:
-        # TODO: this probably fails due being dict(dict( instead of dict(list(dict -- handle this!
-        response_payload = u'Invalid command: {}'.format(parsed_payload['command'])
 
     response = flask.make_response(response_payload, 200)
 
