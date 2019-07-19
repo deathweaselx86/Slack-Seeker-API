fe175d54df5276362260096c74d8b9579a24668c
Trent Robbins <trent.robbins@optimizely.com>
2019-07-18 18:08:23 -0700

    Update help format.

--- app/json_templates.py
+++ app/json_templates.py
@@ -55,7 +55,7 @@ def seeker_help():
                 "type": "section",
                 "text": {
                     "type": "mrkdwn",
-                    "text": "`/seeker search \"[text]\" [tag 1] [tag 2] [tag 3]...`"
+                    "text": "`/seeker search \"[text]\" [tag1] [tag2] [tag3]...`"
                 }
             },
             {
@@ -74,7 +74,7 @@ def seeker_help():
                 "type": "section",
                 "text": {
                     "type": "mrkdwn",
-                    "text": "`/seeker save [message_URL] [tag1] [tag 2] [tag 3]... | [description]`"
+                    "text": "`/seeker save [message_URL] [tag1] [tag2] [tag3]... | [description]`"
                 }
             },
             {
